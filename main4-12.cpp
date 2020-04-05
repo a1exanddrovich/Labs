c2414f0f91b9522c09345ea402bd9f968095d3c9
a1exanddrovich <59010036+a1exanddrovich@users.noreply.github.com>
2020-04-05 21:21:10 +0300

    Update main4-12.cpp

--- main4-12.cpp
+++ main4-12.cpp
@@ -16,12 +16,9 @@ int main() {
 	for (int i = 0; i < SIZE; i++)
 		cin >> arr[i];
 
-	for (int i = 0; i < SIZE - 1; i++) {
-
+	for (int i = 0; i < SIZE - 1; i++)
 		if (arr[i] > arr[i + 1])
 			count++;
-
-	}
 
 	cout << "The number of inversions is " << count << endl;
 
